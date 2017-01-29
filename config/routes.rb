644d1886d5314415ce08f7ebb18d6e6a4f7ec1f4
karuna24s <karuna24s@gmail.com>
2017-01-29 09:59:15 -0500

    update the location of the checklist routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :checklists
-  resources :jobs
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'application#hello'
+  resources :jobs do
+    resources :checklists
+  end
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
