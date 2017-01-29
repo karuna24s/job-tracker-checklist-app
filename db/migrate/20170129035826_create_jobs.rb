71eba08a6c447ceb4a724e004032de8dfe1a1d85
karuna24s <karuna24s@gmail.com>
2017-01-28 23:17:00 -0500

    add resource for jobs

---
+++ db/migrate/20170129035826_create_jobs.rb
@@ -0,0 +1,15 @@
+class CreateJobs < ActiveRecord::Migration[5.0]
+  def change
+    create_table :jobs do |t|
+      t.string :job_title
+      t.string :company
+      t.string :job_description
+      t.string :company_url
+      t.date :date
+      t.integer :status
+      t.string :point_of_contact
+      t.string :job_reference
+      t.timestamps null: false
+    end
+  end
+end
