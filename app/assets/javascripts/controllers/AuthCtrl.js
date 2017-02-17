79b176eb06330cb452a3d2e9fc24f30f759a8289
karuna24s <karuna24s@gmail.com>
2017-02-17 17:58:23 -0500

    set up AuthCtrl.js and login and register views

--- app/assets/javascripts/controllers/AuthCtrl.js
+++ app/assets/javascripts/controllers/AuthCtrl.js
@@ -1,5 +1,23 @@
 angular
   .module('JobTrackerChecklistApp')
-  .controller('AuthCtrl', function($scope, $rootScope, Auth){
+  .controller('AuthCtrl', function($scope, $rootScope, Auth, $state){
+    var config = {headers: {'X-HTTP-Method-Override': 'POST'}}
 
+    $scope.register = function(){
+      Auth.register($scope.user, config).then(function(user){
+        $rootScope.user = user
+        alert("Thanks for signing up, " + user.name);
+        $state.go('home');
+      }, function(errorResponse){
+      });
+    };
+
+    $scope.login = function(){
+      Auth.login($scope.user, config).then(function(user){
+        $rootScope.user = user
+        alert("You're all signed in, " + user.name);
+        $state.go('home');
+      }, function(errorResponse){
+      });
+    }
   })
