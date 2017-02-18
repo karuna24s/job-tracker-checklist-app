43aca928950d580b035aa600774aeb500bed8774
karuna24s <karuna24s@gmail.com>
2017-02-18 10:07:48 -0500

    updating app.js and application_controller

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -4,7 +4,4 @@
 
   angular
     .module('JobTrackerChecklistApp', ['ui.router', 'templates', 'Devise', 'ngMessages'])
-    .config(function($httpProvider){
-      $httpProvider.defaults.headers.common['X-CSRF-Token'] = $('meta[name=csrf-token]').attr('content');
-    });
 }())
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :configure_permitted_parameters, if: :devise_controller?
-  skip_before_action :verify_authenticity_token, if: :json_request?
-  after_action :set_csrf_cookie_for_ng
+  skip_before_action :verify_authenticity_token
 
   respond_to :json
 
@@ -10,23 +9,10 @@ class ApplicationController < ActionController::Base
     render 'application/index'
   end
 
-
-  def set_csrf_cookie_for_ng
-    cookies['XSRF-TOKEN'] = form_authenticity_token if protect_against_forgery?
-  end
-
   protected
 
-  def json_request?
-    request.format.json?
-  end
-
-  def verified_request?
-    super || valid_authenticity_token?(session, request.headers['X-XSRF-TOKEN'])
-  end
-
   def configure_permitted_parameters
-    added_attrs = [:username, :email, :password, :password_confirmation, :remember_me]
+    added_attrs = [:name, :email, :password, :password_confirmation, :remember_me]
     devise_parameter_sanitizer.permit :sign_up, keys: added_attrs
     devise_parameter_sanitizer.permit :account_update, keys: added_attrs
   end
